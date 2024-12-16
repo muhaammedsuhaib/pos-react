cced65cbb04adc39ed154c8325c25d13d8761239
muhaammedsuhaib <muhammedsuhaibpottayil@gmail.com>
2024-12-16 17:31:02 +0530

    ...

--- src/components/dashboradSalesComponents/dashboardSalesReports/OrderSummary.jsx
+++ src/components/dashboradSalesComponents/dashboardSalesReports/OrderSummary.jsx
@@ -6,17 +6,22 @@ import Chart from "react-apexcharts";
 import { useDispatch, useSelector } from "react-redux";
 import { selectOrderSummary } from "../../../reducer/sales/reducer";
 import { listOrderSummary } from "../../../reducer/sales/actions";
-import { base_url, getLoginToken } from "../../utils/utils";
-import axios from "axios";
+import useExportPdf from "./hooks/useExportPdf";
+import LoadingOverlay from "./components/LoadingOverlay";
 
 function OrderSummary() {
+  const { isSavingPdf, exportPdf } = useExportPdf();
   const dispatch = useDispatch();
   const orderdatas = useSelector(selectOrderSummary);
   const orderSummardata = orderdatas?.data;
 
   const currentDate = dayjs();
   const [selectedDate, setSelectedDate] = useState([currentDate, currentDate]);
-  const [isSvavingPdf, setIsSavingPdf] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("summary");
+
+  const handleChange = (value) => {
+    setSelectedOption(value);
+  };
 
   const chartOptions = {
     chart: {
@@ -37,7 +42,7 @@ function OrderSummary() {
     orderSummardata?.dineIn?.count,
     orderSummardata?.takeAway?.count,
     orderSummardata?.booking?.count,
-    orderSummardata?.overallSales?.count,
+    orderSummardata?.other?.count,
   ];
 
   const convertDateFormat = (date) => {
@@ -60,29 +65,19 @@ function OrderSummary() {
     fetchSOrdersummary();
   }, [selectedDate, dispatch]);
 
-  const handleExportPdf = async () => {
-    const formattedStartDate = convertDateFormat(selectedDate[0]);
-    const formattedEndDate = convertDateFormat(selectedDate[1]);
-    setIsSavingPdf(true);
+  const handleExportPdf = () => {
     try {
-      const url = `${base_url}/sales/get-order-summary-url`;
-      const response = await axios.get(url, {
-        params: {
-          date: { startDate: formattedStartDate, endDate: formattedEndDate }
-        },
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          Authorization: `Bearer ${getLoginToken()}`,
-        },
-      });
-      if (response.data && response.data.data) {
-        window.open(response.data.data, "_blank");
-      }
-      setIsSavingPdf(false);
+      const formattedStartDate = convertDateFormat(selectedDate[0]);
+      const formattedEndDate = convertDateFormat(selectedDate[1]);
+
+      exportPdf(
+        formattedStartDate,
+        formattedEndDate,
+        selectedOption,
+        "get-order-summary-url"
+      );
     } catch (error) {
-      // console.log(error, 'error pdf');
-      setIsSavingPdf(false);
+      console.log("Export error sales overall", error);
     }
   };
 
@@ -275,11 +270,21 @@ function OrderSummary() {
             <div className="w-[100%] grid grid-flow-row grid-cols-3 gap-[20px] pb-[10px]">
               <div className="flex flex-col gap-y-[20px] justify-between">
                 <div className="flex items-center space-x-[10px]">
-                  <Radio className="custom-black-radio" />
+                  <Radio
+                    className="custom-black-radio"
+                    value="summary"
+                    checked={selectedOption === "summary"}
+                    onChange={() => handleChange("summary")}
+                  />
                   <h1>Summary</h1>
                 </div>
                 <div className="flex items-center space-x-[10px]">
-                  <Radio className="custom-black-radio" />
+                  <Radio
+                    className="custom-black-radio"
+                    value="details"
+                    checked={selectedOption === "details"}
+                    onChange={() => handleChange("details")}
+                  />
                   <h1>Details</h1>
                 </div>
               </div>
@@ -387,11 +392,7 @@ function OrderSummary() {
           </div>
         </div>
       </div>
-      {isSvavingPdf && (
-        <div className="absolute top-0 left-0 w-full flex items-center justify-center h-screen bg-[#0000002f]">
-          <div className="w-10 h-10 border-4 border-dashed rounded-full animate-spin border-[#F95433]"></div>
-        </div>
-      )}
+      {isSavingPdf && <LoadingOverlay />}
     </div>
   );
 }
